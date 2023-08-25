513ea100dfabb206948bf2fb631ac8478ea8c2c0
artem <artemfr@hotmail.com>
2023-08-25 22:07:27 +0200

    partially added 3rd section and some javascript code for highlighting current section when we scroll

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// const sections = document.querySelectorAll('section');
+// const nav = document.querySelectorAll('menu');
+
+// window.addEventListener('scroll',  ()=>{
+//     let current = ' ';
+//     sections.forEach( section => {
+//         const sectionTop = section.offsetTop;
+//         const sectionHeight = section.clientHeight;
+//         if(scrollY >= sectionTop){
+//             current = section.getAttribute('id');
+//         }
+//     })
+//     nav.forEach (h2 =>{
+//         h2.classList.remove('active');
+//     })
+// })
