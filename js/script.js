f1eb3f02c3e1ae9157b5be645573339f5bc5f03f
artem <artemfr@hotmail.com>
2023-08-28 23:57:39 +0200

    Little modifications

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// document.querySelector('input[role="send"]').click(function () {
+//     let document.querySelector = document.querySelector(this);
+//     var hasClass = document.querySelectort.hasClass('done');
+//     if (!hasClass){
+//       document.querySelector(this).addClass('clicked');
+//       setTimeout(function(){
+//         document.querySelectort.removeClass('clicked').addClass('done').attr({
+//           value: '',
+//         });
+//       }, time()); 
+//     } else if (hasClass){
+//       document.querySelectort.removeClass().attr({
+//         value: 'Send',
+//       });
+//     }
+//   });
+  
+//   function time() {
+//     return 3000 + Math.random() * 2000
+//   }
