ace09789838d72b5fca32fec6164029fbfdaef8a
artem <artemfr@hotmail.com>
2023-09-05 02:22:41 +0200

    Added new popup for image and changing js properties

--- js/script.js
+++ js/script.js
@@ -1,7 +1,22 @@
 const button = document.getElementById("details");
 const button2 = document.getElementById("back-button");
+
+
+const image = document.querySelector('.fa-image');
+const modalOpened = document.querySelector('.modal-container');
+
+image.addEventListener('click', showImage);
 button.addEventListener('click', flipCard);
-button2.addEventListener('click', flipCard)
+button2.addEventListener('click', flipCard);
+
 function flipCard() {
     card.classList.toggle("flipCard")
+}
+
+function showImage() {
+  modalOpened.style.display = "flex";
+  body.style.opacity = "0.8";
+}
+function closeImage() {
+    modalOpened.style.display = "none"
 }
