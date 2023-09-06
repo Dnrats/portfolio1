e7e90160f6f85386abb9b806f8f093edc9300c55
artem <artemfr@hotmail.com>
2023-09-06 14:57:58 +0200

    I didnt commit for 3 days fighting agtainst cards and popups. But things should be better now. Cards are working fine. And im worknig on popup images now

--- js/script.js
+++ js/script.js
@@ -1,24 +1,45 @@
-const button = document.getElementById("details");
-const button2 = document.getElementById("back-button");
-
-
-const image = document.querySelector('.fa-image');
-const modalOpened = document.querySelector('.modal-container');
-const closeButton = document.querySelector('.fa-xmark');
-
-closeButton.addEventListener('click', hideImage);
-image.addEventListener('click', showImage);
-button.addEventListener('click', flipCard);
-button2.addEventListener('click', flipCard);
-
+let buttons = document.querySelectorAll('.buttons')
+buttons.forEach(button => {
+   button.addEventListener('click', flipCard);
+ });
 function flipCard() {
-    card.classList.toggle("flipCard")
+   card.classList.toggle('flipCard')
 }
+function flipCard(button) {
+   // Find the parent card element of the clicked button
+   const card = button.closest('.card');
+   
+   // Toggle the 'flipCard' class for the specific card
+   card.classList.toggle('flipCard');
+ }
 
-function showImage() {
-  modalOpened.style.display = "flex";
-  body.style.opacity = "0.8";
+
+ const openPopupButton = document.getElementById('open-popup');
+ const closePopupButton = document.getElementById('close-popup');
+ const overlay = document.getElementById('overlay');
+ const popup = document.getElementById('popup');
+ 
+ // Function to open the popup
+ function openPopup() {
+   overlay.style.display = 'block';
+
+   // Add the "show" class to trigger the transition animation
+   popup.classList.add('show');
+
+   document.body.style.overflow = 'hidden'; // Disable scrolling
 }
-function hideImage() {
-    modalOpened.style.display = "none"
+ 
+ // Function to close the popup
+ function closePopup() {
+   overlay.style.display = 'none';
+   
+   // Remove the "show" class to trigger the fade-out animation
+   popup.classList.remove('show');
+   
+   document.body.style.overflow = 'auto'; // Enable scrolling
 }
+ 
+ // Event listeners
+ openPopupButton.addEventListener('click', openPopup);
+ closePopupButton.addEventListener('click', closePopup);
+ overlay.addEventListener('click', closePopup)
