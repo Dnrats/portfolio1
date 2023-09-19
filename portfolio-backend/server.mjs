0246d2063e19270e3500490965531e96a31c5c46
artem <artemfr@hotmail.com>
2023-09-19 19:51:00 +0200

    Im having problems with login. Specifically i was stuck for some time on login because of hashing, i decided to remove it for some time, and added app.use session with a random password. So for now its visible, but i'll change it later. Its purely for the test purposes

--- portfolio-backend/server.mjs
+++ portfolio-backend/server.mjs
@@ -4,18 +4,30 @@ import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import mysql from 'mysql';
 import path from 'path';
-import bcrypt from 'bcrypt';
-
-// Import the configuration
-import { dbConfig } from './config.js'; // Adjust the path as needed
+// import bcrypt from 'bcrypt';
+import bodyParser from 'body-parser'; 
+import { dbConfig } from './config.js'; 
+import session from 'express-session'; // Import express-session
 
 // Obtain the directory path of the current module
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+
 // Create an Express app and set up the port
 const app = express();
 const port = 3000;
+
+// Configure body-parser to handle JSON data
+app.use(bodyParser.json());
+
+app.use(
+  session({
+    secret: 'vkwefbdn&*f67mjfhNfkFN%', // Replace with a secret key for session encryption
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 // Define a route for the homepage
 app.get('/', (req, res) => {
@@ -28,6 +40,7 @@ app.use('/Css', express.static(path.join(__dirname, 'Css')));
 // POST route for handling user login
 app.post('/login', (req, res) => {
   const { username, password } = req.body;
+  console.log('Stage 1');
 
   // Query the database to verify admin user's credentials
 db.query('SELECT * FROM admin_user WHERE username = ?', [username], async (err, results) => {
@@ -39,24 +52,29 @@ db.query('SELECT * FROM admin_user WHERE username = ?', [username], async (err, 
   if (results.length === 0) {
     return res.status(401).json({ success: false, message: 'Authentication failed' });
   }
+console.log('Stage 2');
 
   // Compare the hashed password with the provided password
-  const storedPasswordHash = results[0].password; 
-  const passwordsMatch = await bcrypt.compare(password, storedPasswordHash);
+  // const storedPassword = results[0].password; 
+  // const passwordsMatch = await bcrypt.compare(password, storedPasswordHash);
 
-  if (!passwordsMatch) {
+  // if (!passwordsMatch) {
+  //   console.log('Password does not match');
+  //   return res.status(401).json({ success: false, message: 'Authentication failed' });
+  // }
+  
+  if (password === results[0].password) {
+    console.log('Authentication successful');
+    req.session.username = username;
+    return res.status(200).json({ success: true, message: 'Authentication successful' });
+  } else {
+    console.log('Password does not match');
     return res.status(401).json({ success: false, message: 'Authentication failed' });
   }
+});
+});
+console.log('Stage 4');
 
-  // User is authenticated
-  res.status(200).json({ success: true, message: 'Authentication successful' });
-});
-});
-
-// Send to dashboard if connection is successful
-app.get('/dashboard', isAuthenticated, (req, res) => {
-  res.sendFile(path.join(__dirname, 'dashboard.html'));
-});
 
 // MySQL database connection setup using the imported configuration
 const db = mysql.createConnection(dbConfig);
@@ -65,28 +83,27 @@ const db = mysql.createConnection(dbConfig);
 function isAuthenticated(req, res, next) {
   const { username, password } = req.body;
 
-  // Query the database to verify admin user's credentials
-  db.query('SELECT * FROM admin_users WHERE username = ?', [username], async (err, results) => {
-    if (err) {
-      console.error('Database error:', err);
-      return res.status(500).json({ message: 'Database error' });
-    }
+  // Compare the provided username and password with the user's credentials stored in the session
+  const storedUsername = req.session.username; // Access the username from the session
+  const storedPassword = req.session.password; // Access the plain text password from the session
 
-    if (results.length === 0) {
-      return res.status(401).json({ message: 'Unauthorized' });
-    }
+  if (username === storedUsername && password === storedPassword) {
+    // Assuming you have successfully authenticated the user
+      req.session.username = username; 
+// Store the username in the session
 
-    const storedPasswordHash = results[0].password;
-    const passwordsMatch = await bcrypt.compare(password, storedPasswordHash);
-
-    if (!passwordsMatch) {
-      return res.status(401).json({ message: 'Unauthorized' });
-    }
-
-    // User is authenticated
+    // User is authenticated, proceed to the next middleware
     next();
-  });
+  } else {
+    // User is not authenticated, send an unauthorized response
+    res.status(401).json({ message: 'Unauthorized' });
+  }
 }
+
+// Send to dashboard if connection is successful
+app.get('/dashboard', isAuthenticated, (req, res) => {
+  res.sendFile(path.join(__dirname, 'dashboard.html'));
+});
 
 
 // Start the server
@@ -136,5 +153,4 @@ app.get('/logout', (req, res) => {
 app.use(express.static(path.join(__dirname)));
 
 app.use('/Javascript', express.static(path.join(__dirname, 'Javascript')));
-
 
