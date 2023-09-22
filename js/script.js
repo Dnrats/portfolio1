464a1c40bf959fad4719dbcbee55ab46bc46d2e2
artem <artemfr@hotmail.com>
2023-09-22 03:12:29 +0200

    Fixed the problem with icons not fading in one after another correctly

--- js/script.js
+++ js/script.js
@@ -59,33 +59,34 @@ function flipCard(button) {
 //  ICONS CONTAINER
 
 const showTechnologiesButton = document.getElementById('showTechnologies');
-const hiddenDiv = document.getElementById('hiddenDiv');
-const images = hiddenDiv.querySelectorAll('img');
+    const hiddenDiv = document.getElementById('hiddenDiv');
+    const images = hiddenDiv.querySelectorAll('img');
 
-let isHidden = true;
+    let isHidden = true;
 
-showTechnologiesButton.addEventListener('click', () => {
-    isHidden = !isHidden;
-    hiddenDiv.style.display = isHidden ? 'none' : 'flex';
-    showTechnologiesButton.textContent = isHidden ? 'Show' : 'Hide';
+    showTechnologiesButton.addEventListener('click', () => {
+        isHidden = !isHidden;
+        hiddenDiv.style.display = isHidden ? 'none' : 'flex';
+        showTechnologiesButton.textContent = isHidden ? 'Show Stack' : 'Hide';
 
-    if (!isHidden) {
-        // If showing, start fading in the images
-        images.forEach((image, index) => {
+        if (!isHidden) {
+            // If showing, reset the image opacity and blur
+            images.forEach((image) => {
+                image.style.opacity = 0;
+                image.style.filter = 'blur(10px)';
+            });
+
+            // After a small delay, start fading in the images sequentially from left to right
             setTimeout(() => {
-                image.style.opacity = 1;
-                image.style.filter = 'blur(0)';
-            }, index * 400); // Adjust the delay between each image fading in
-        });
-    } else {
-        // If hiding, reset the image opacity and blur
-        images.forEach((image) => {
-            image.style.opacity = 0;
-            image.style.filter = 'blur(10px)';
-        });
-    }
-});
-
+                images.forEach((image, index) => {
+                    setTimeout(() => {
+                        image.style.opacity = 1;
+                        image.style.filter = 'blur(0)';
+                    }, index * 400); // Adjust the delay between each image fading in
+                });
+            }, 50); // Delay before starting the fade-in effect
+        }
+    });
 
 
 // CHAT WIDGET
