79b42c562203b1cf816da895ea08a4a5c7a33e9a
artem <artemfr@hotmail.com>
2024-01-26 15:48:05 +0100

    Added slider

--- js/script.js
+++ js/script.js
@@ -1,93 +1,20 @@
-let buttons = document.querySelectorAll('.buttons')
-buttons.forEach(button => {
-   button.addEventListener('click', flipCard);
- });
-function flipCard(button) {
-   // Find the parent card element of the clicked button
-   const card = button.closest('.card');
-   
-   // Toggle the 'flipCard' class for the specific card
-   card.classList.toggle('flipCard');
- }
-
-
- const imageSources = [
-   './assets/Happy-Health1.png',
-   './assets/Contact-form.png',
-   './assets/Marvel-screenshot.png',
-   './assets/jeu-batons.png',
-   'path_to_screenshot_5.jpg',
-   'path_to_screenshot_6.jpg',
-   'path_to_screenshot_7.jpg',
-   'path_to_screenshot_8.jpg',
-   './assets/Sabor-del-mundo.png',
- ];
-
- const imageIcons = document.querySelectorAll('.card .fa-regular.fa-image');
-
- // Get the popup container and content
- const popupContainer = document.getElementById('popup-container');
- const popupContent = document.getElementById('popup-content');
- const screenshot = document.getElementById('screenshot');
- const closePopup = document.getElementById('close-popup');
- 
- // Add click event listeners to each image icon
- imageIcons.forEach((icon, index) => {
-   icon.addEventListener('click', () => {
-     // Get the data-target value to identify the specific popup
-     const target = icon.getAttribute('data-target');
- 
-     // Get the index of the target
-     const targetIndex = parseInt(target) - 1; // Subtract 1 to account for zero-based index
- 
-     // Load the screenshot for the clicked card
-     const screenshotSrc = imageSources[targetIndex];
-     screenshot.setAttribute('src', screenshotSrc);
- 
-     // Add the "active" class to trigger the animation
-     popupContainer.classList.add('active');
-   });
- });
- 
- // Add click event listener to close the popup
- closePopup.addEventListener('click', () => {
-   // Remove the "active" class to hide the popup with animation
-   popupContainer.classList.remove('active');
- });
-
-
-//  ICONS CONTAINER
-
-const showTechnologiesButton = document.getElementById('showTechnologies');
-    const hiddenDiv = document.getElementById('hiddenDiv');
-    const images = hiddenDiv.querySelectorAll('img');
-
-    let isHidden = true;
-
-    showTechnologiesButton.addEventListener('click', () => {
-        isHidden = !isHidden;
-        hiddenDiv.style.display = isHidden ? 'none' : 'flex';
-        showTechnologiesButton.textContent = isHidden ? 'Show Stack' : 'Hide';
-
-        if (!isHidden) {
-            // If showing, reset the image opacity and blur
-            images.forEach((image) => {
-                image.style.opacity = 0;
-                image.style.filter = 'blur(10px)';
-            });
-
-            // After a small delay, start fading in the images sequentially from left to right
-            setTimeout(() => {
-                images.forEach((image, index) => {
-                    setTimeout(() => {
-                        image.style.opacity = 1;
-                        image.style.filter = 'blur(0)';
-                    }, index * 400); // Adjust the delay between each image fading in
-                });
-            }, 50); // Delay before starting the fade-in effect
-        }
-    });
-
+const swiper = new Swiper('.swiper-container', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+     rotate: 50, // Set rotation angle
+     stretch: 0, // Set how much to stretch the slide
+     depth: 100, // Set depth of the effect (higher value = more pronounced effect)
+     modifier: 1, // Set modifier of the effect
+     slideShadows: true, // Enable slide shadows
+  },
+  pagination: {
+     el: '.swiper-pagination',
+     clickable: true,
+  },
+});
 
 // CHAT WIDGET
 
